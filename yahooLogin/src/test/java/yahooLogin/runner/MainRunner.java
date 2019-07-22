912fbca14e26dff0bdde569c8e4542bd93bf8c54
new <new@Aashiya.fios-router.home>
2019-07-22 17:45:11 -0400

    Runner

--- yahooLogin/src/test/java/yahooLogin/runner/MainRunner.java
+++ yahooLogin/src/test/java/yahooLogin/runner/MainRunner.java
@@ -1,5 +1,41 @@
 package yahooLogin.runner;
 
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+
+		features = { "src/test/java/NewTour_logjn/features" },
+		glue = { "NewTour_logjn.stepDefination" },
+
+		monochrome = true, tags = {},
+
+		// plugin = {
+		// "com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html"}
+
+		// plugin = ("json:target/cucumber-reports/CucumberTestReport.json")
+
+		// plugin
+		// ={"pretty","html:target/cucumber","json:target/cucumber-reports/cucumber.json",
+
+		// "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
+
+		//format = { "pretty", "html:test-output_1",
+				//"json:target/cucumber-reports/CucumberTestReport.json" }, 
+		//plugin = ("json:target/cucumber-reports/CucumberTestReport.json")
+
+		plugin = {"pretty", "html:target/cucumber", "json:target/cucumber-reports/cucumber.json"}
+)
+
+
+
+
+
+
+
 public class MainRunner {
 
 }
